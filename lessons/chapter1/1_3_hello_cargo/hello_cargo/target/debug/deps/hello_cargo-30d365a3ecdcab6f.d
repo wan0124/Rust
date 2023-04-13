a5efb8d34970baea8ba8999ac3decbb9070e3284
Wan <jcps3503@gmail.com>
2023-04-13 10:17:25 +0800

    finish chapter1

---
+++ lessons/chapter1/1_3_hello_cargo/hello_cargo/target/debug/deps/hello_cargo-30d365a3ecdcab6f.d
@@ -0,0 +1,5 @@
+/Users/wan/github/Rust/lessons/chapter1/1_3_hello_cargo/hello_cargo/target/debug/deps/hello_cargo-30d365a3ecdcab6f.rmeta: src/main.rs
+
+/Users/wan/github/Rust/lessons/chapter1/1_3_hello_cargo/hello_cargo/target/debug/deps/hello_cargo-30d365a3ecdcab6f.d: src/main.rs
+
+src/main.rs:
---
+++ lessons/chapter1/1_3_hello_cargo/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/wan/github/Rust/lessons/chapter1/1_3_hello_cargo/hello_cargo/target/debug/hello_cargo: /Users/wan/github/Rust/lessons/chapter1/1_3_hello_cargo/hello_cargo/src/main.rs
